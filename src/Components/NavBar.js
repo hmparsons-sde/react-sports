1da2691b175dfa277d2a10656128da21460e8abd
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-05-07 17:24:13 -0500

    added images

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -31,7 +31,7 @@ const NavBar = ({ user }) => {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">WOO!</NavbarBrand>
+        <NavbarBrand href="/">Wrestlemania Throwback</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
