ac658a08cba69edd93c7adb8c9676fdc77c056d1
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-05-06 20:35:41 -0500

    user auth

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { getWrestler } from '../helpers/data/wrestlerData';
 import './App.scss';
 import Routes from '../helpers/Routes';
@@ -7,9 +9,24 @@ import NavBar from '../Components/NavBar';
 
 function App() {
   const [wrestlers, setWrestlers] = useState([]);
+  const [user, setUser] = ([]);
 
   useEffect(() => {
-    getWrestler().then(setWrestlers);
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userInfoObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0]
+        };
+        getWrestler(authed.uid).then((wrestlersArray) => setWrestlers(wrestlersArray));
+        setUser(userInfoObj);
+      } else if (user || user === null) {
+        setUser(false);
+        setWrestlers([]);
+      }
+    });
   }, []);
 
   return (
